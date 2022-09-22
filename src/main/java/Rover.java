eca5bf618ab1448b369acbf9e1d0160057c5d835
Ivan Cozar Laguna <ivan.cozar@babelgroup.com>
2022-09-22 15:56:13 +0200

    Added Rover test

---
+++ src/main/java/Rover.java
@@ -0,0 +1,12 @@
+public class Rover{
+
+    public void rotate(String i){
+    }
+
+    public String getOrientation(){
+        return "O";
+    }
+
+    public void setOrientation(String n){
+    }
+}
---
+++ src/test/java/RoverTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RoverTest{
+
+    @Test
+    public void whenOrientationIsNorthAndCommandIsIThenOrientationShouldBeWest(){
+        Rover rover = new Rover();
+        rover.setOrientation("N");
+
+        rover.rotate("I");
+
+        Assertions.assertEquals("O", rover.getOrientation());
+    }
+
+}
